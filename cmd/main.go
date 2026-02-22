3ecc3519af653ac22e89529dde71e23ee59f702d
surush3005 <surush3005@gmail.com>
2026-02-22 21:41:26 +0500

    change main go

--- cmd/main.go
+++ cmd/main.go
@@ -8,34 +8,35 @@ import (
 )
 
 func main() {
-	//задание 7.1 Зачисление средств
-	crd := &types.Card{
-		Id:       0001,
-		PAN:      "5058 XXXX XXXX 9999",
-		Balance:  -100,
-		Currency: types.TJS,
-		Color:    "Black",
-		Name:     "MyCard",
-		Active:   true,
-	}
+	/*
+		//задание 7.1 Зачисление средств
+		crd := &types.Card{
+			Id:       0001,
+			PAN:      "5058 XXXX XXXX 9999",
+			Balance:  -100,
+			Currency: types.TJS,
+			Color:    "Black",
+			Name:     "MyCard",
+			Active:   true,
+		}
 
-	card.Deposit(crd, 5000)
+		card.Deposit(crd, 5000)
 
-	fmt.Println("Новый баланс:", crd.Balance)
-
+		fmt.Println("Новый баланс:", crd.Balance)
+	*/
 	//задание 7.2 Процент на остаток
 	bonus := &types.Card{
 		Id:         0002,
 		PAN:        "5058 XXXX XXXX 9999",
-		Balance: 	100,
-		MinBalance: 3000,
+		Balance:    100,
+		MinBalance: 10_000,
 		Currency:   types.TJS,
 		Color:      "Black",
 		Name:       "MyCard",
 		Active:     true,
 	}
 
-	card.AddBonus(bonus, 3, 30, 365)
+	card.AddBonus(bonus, 5, 30, 365)
 
 	fmt.Println("Доход:", bonus.MinBalance)
 }
