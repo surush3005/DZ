c63f6eae31c034761fab86df40a990e748ad4a21
surush3005 <surush3005@gmail.com>
2026-02-22 20:31:12 +0500

    ss

--- cmd/main.go
+++ cmd/main.go
@@ -4,11 +4,12 @@ import (
 	"fmt"
 
 	"github.com/surush3005/DZ.git/pkg/types"
+	"github.com/surush3005/DZ.git/pkg/card"
 )
 
 func main() {
 
-	crd := &types.Card{
+	cards := &types.Card{
 		Id:       0001,
 		PAN:      "5058 XXXX XXXX 9999",
 		Balance:  -100,
@@ -18,7 +19,7 @@ func main() {
 		Active:   true,
 	}
 
-	Deposit(crd, 5000)
+	card.Deposit(card,5000)
 
-	fmt.Println("Новый баланс:", crd.Balance)
+	fmt.Println("Новый баланс:", cards.Balance)
 }
